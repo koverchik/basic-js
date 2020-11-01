c29da058ec9e2a0d10972f4f57447dfeb5b8d08c
koverchik <koverchik.o@gmail.com>
2020-11-01 21:40:25 +0300

    feat: calculate Depth function worked only in local file

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
+  calculateDepth(arr) {
+    let result = 0;
+    function caunt(array) {
+      for (var i = 0; i < arr.length; i++) {
+        if(Array.isArray(arr[i])){
+            result += 1;
+          }
+        }
+    }
+    if (Array.isArray(arr)) {
+    result += 1;
+      caunt(arr)
+      }
+
+    return result;
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
   }
 };
