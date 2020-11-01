bcb27fcc3e5d4117312203a6f236ddd1071844e9
koverchik <koverchik.o@gmail.com>
2020-11-01 12:30:28 +0300

    feat: carbon dating no solve  -  should return false on wrong type  -  should handle inadequate values

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  const lambda = 0.693;
+  if(arguments.length == 0 || sampleActivity == null || isNaN(sampleActivity)){
+    return false;
+  }else {
+    let value = Number(sampleActivity);
+    return Math.ceil(HALF_LIFE_PERIOD/MODERN_ACTIVITY*lambda*value);
+  }
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
